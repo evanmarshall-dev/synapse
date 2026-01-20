389c5f825722b05a004edca338609899daabb485
jalmontegit <jalmonte721@gmail.com>
2026-01-20 18:47:10 -0500

    setting up authentication, log in added at home page and authenticated nav links

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -7,7 +8,7 @@ class Post(models.Model):
     program_type = models.CharField(max_length=100, null=True, blank=True)
     grad_year = models.IntegerField(null=True, blank=True)
     linkedin_url = models.URLField(null=True, blank=True)
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
