e3c50881db36380243481a5f9e73ca9a8996b113
makadana <mrmkmtch@outlook.com>
2026-01-21 08:27:38 -0500

    Add edit and delete functionality for posts

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,6 @@ from main_app import views
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('create/', views.PostCreate.as_view(), name='post_create'),
+    path('<int:pk>/update/', views.PostUpdate.as_view(), name='post_update'),
+    path('<int:pk>/delete/', views.PostDelete.as_view(), name='post_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,8 @@
 from django.contrib.auth.views import LoginView
 from main_app.models import Post
 from main_app.forms import PostForm
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 # from django.contrib.auth.mixins import LoginRequiredMixin
 
 class Home(LoginView):
@@ -14,3 +15,15 @@ class PostCreate(CreateView):
     form_class = PostForm
     template_name = 'posts/post_form.html'
     # success_url = '/'
+
+# class PostUpdate(LoginRequiredMixin, UpdateView):
+class PostUpdate(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'posts/post_form.html'
+
+# class PostDelete(LoginRequiredMixin, DeleteView):
+class PostDelete(DeleteView):
+    model = Post
+    template_name = 'posts/post_confirm_delete.html'
+    success_url = reverse_lazy('home')
